951457c9b38f82ffb94c4691de901bad3565356a
pFuzzy <p.fazii888@gmail.com>
2021-03-22 10:52:19 +0100

    initial commit

---
+++ src/main/java/com/codecool/reactomonbackend/ReactomonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.reactomonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactomonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactomonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/reactomonbackend/ReactomonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.reactomonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactomonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
