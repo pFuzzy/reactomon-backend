077f9761f7810baf330932b0abc6e222a2c7bcaf
pFuzzy <p.fazii888@gmail.com>
2021-03-22 14:29:57 +0100

    add CommandLineRunner

--- src/main/java/com/codecool/reactomonbackend/ReactomonBackendApplication.java
+++ src/main/java/com/codecool/reactomonbackend/ReactomonBackendApplication.java
@@ -1,7 +1,12 @@
 package com.codecool.reactomonbackend;
 
+import com.codecool.reactomonbackend.service.ApiConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ReactomonBackendApplication {
@@ -10,4 +15,13 @@ public class ReactomonBackendApplication {
         SpringApplication.run(ReactomonBackendApplication.class, args);
     }
 
+    @Autowired
+    private ApiConverter apiConverter;
+
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+            System.out.println(apiConverter.createEntityFromApiData(1));
+        };}
 }
